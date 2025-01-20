f70de148225573dd1c763b300fd088d395cd4e8e
plafet <plafett@gmail.com>
2025-01-20 14:07:29 +0300

    Create script_helm.sh

---
+++ scripts/script_helm.sh
@@ -0,0 +1,4 @@
+#Скачайте нужную вам версию https://github.com/helm/helm/releases
+#Распакуйте её tar -zxvf
+#Найдите helm binary файл в директории из распаковки, и переместите в нужное место mv linux-amd64/helm /usr/local/bin/helm
+# helm help проверить работу help
